6831d0f0976ac63e53edb74f97a2b10a345faf4f
ehron <ehron@teamgwinn.com>
2018-06-03 10:57:25 -0400

    fix ports

--- upstream.py
+++ upstream.py
@@ -3,8 +3,8 @@
 # At some point this could be dynamic.
 # But for now, use the local proxy
 def get_listing_addr():
-    return 'http://localhost:10001/listing'
+    return 'http://localhost:10002/listing'
 
 
 def get_product_addr():
-    return 'http://localhost:10002/product'
+    return 'http://localhost:10001/product'
