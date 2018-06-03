5f46c50d699d4ca1a95083bca579519073b1bf2e
ehron <ehron@teamgwinn.com>
2018-06-03 13:40:23 -0400

    put templates in right place

--- client.py
+++ client.py
@@ -21,30 +21,11 @@ def get_listings():
 def healthz():
     return 'OK'
 
-template = """<html>
-      <body>
-        <h1>Welcome!</h2>
-        <h3>Products:</h3>
-        <ul>
-            {% for prod in prod_list %}
-              <li>prod</li>
-            {% endfor %}
-        </ul>
-        <h3>Listings:</h3>
-        <ul>
-            {% for listing in listings_list %}
-              <li>prod</li>
-            {% endfor %}
-        </ul>
-      </body>
-    </html>
-"""
-
 @app.route('/')
 def index():
     products = get_products()
     listings = get_listings()
-    return render_template(template, prod_list=products, listings_list=listings)
+    return render_template('index.html', prod_list=products, listings_list=listings)
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=8080)
