e9cf9b8f130b90bde58511c94f3f4992db030913
ehron <ehron@teamgwinn.com>
2018-06-03 08:48:51 -0400

    correct API URLs

--- upstream.py
+++ upstream.py
@@ -3,8 +3,8 @@
 # At some point this could be dynamic.
 # But for now, use the local proxy
 def get_listing_addr():
-    return 'http://localhost:10001'
+    return 'http://localhost:10001/listing'
 
 
 def get_product_addr():
-    return 'http://localhost:10002'
+    return 'http://localhost:10002/product'
