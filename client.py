da269df754441acbd743876d4ce7e8dcc2712d26
Thomas Kula <thomas@hashicorp.com>
2018-08-20 12:27:42 -0400

    Use IS_CONNECT to distinguish between Consul Connect and non-Connect

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask import render_template
 import requests
@@ -31,7 +33,9 @@ def healthz():
 def index():
     products = get_products()
     listings = get_listings()
-    return render_template('index.html', prod_list=products, listings_list=listings)
+    is_connect = os.environ.get('IS_CONNECT', 0)
+    return render_template('index.html', prod_list=products, listings_list=listings,
+            is_connect=is_connect)
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=8080)
